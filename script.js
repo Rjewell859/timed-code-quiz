674755f3e7f1bf7ae466ef5b2aaeae13f59893f9
Headwall Forest <headwallforest27@gmail.com>
2022-07-28 18:21:52 -0400

    Worked on javaScript and started css.

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ var introParagraph = document.querySelector("#startparagraph");
 var startButton = document.querySelector("#start");
 var optionButtons = document.querySelector("#options");
 var submitButton = document.querySelector("#submit");
+var initials = document.querySelector("#initials");
 var highScores = document.querySelector("#highscores");
 var choiceResult = document.querySelector("#result");
+var questionsWords = document.querySelector("#questionWords")
+var toAnswer = document.querySelector("#left");
 
 var questionIndex = 0;
 var seconds = 100;
+var questionsLeft;
 
 var option1 = document.createElement("button");
 var option2 = document.createElement("button");
 var option3 = document.createElement("button");
 var option4 = document.createElement("button");
 
+submitButton.style.display = 'none';
+
 var questions = [
     {
         content: "What does HTML stand for?",
         answer: "Hyper Text Markup Language",
+        correct: "B",
         choices: [
-            "Hyper Transfer Marking Language",
-            "Hyper Text Markup Language",
-            "Hard Type Manufactered Live",
-            "Handy Tools Made Locally"
+            A = "Hyper Transfer Marking Language",
+            B = "Hyper Text Markup Language",
+            C = "Hard Type Manufactered Live",
+            D = "Handy Tools Made Locally"
         ]
 
     },
@@ -90,33 +97,94 @@ var questions = [
         ]
     },
     {
-        content: "What is 9 / 3?",
-        answer: "3",
+        content: "What is the '!' symbol in javaScript?",
+        answer: "'!' is the logical not operator.",
+        choices: [
+            "Used at the end of a query selection.",
+            "It is a tool used to remove the first element of an array.",
+            "'!' is the logical not operator.",
+            "The exclamation mark forces a function to execute."
+            
+        ]
+    },
+    {
+        content: "What is the 'this' keyword?",
+        answer: "This refers to the object that a function is a property of.",
         choices: [
             "3",
             "0",
-            "14",
+            "This refers to the object that a function is a property of.",
             "6"
         ]
     },
     {
-        content: "What is 9 / 3?",
-        answer: "3",
+        content: "What are the main data types in javaScript?",
+        answer: "String, number, bigint, boolean, undefined",
+        choices: [
+            "Decimal, integer, undefined, function, long",
+            "String, number, bigint, boolean, undefined",
+            "Var, string, symbol, object, small",
+            "console, form, object, loop"
+        ]
+    },
+    {
+        content: "What is the 'this' keyword?",
+        answer: "This refers to the object that a function is a property of.",
         choices: [
             "3",
             "0",
-            "14",
+            "This refers to the object that a function is a property of.",
             "6"
         ]
     },
     {
-        content: "What is 9 / 3?",
-        answer: "3",
+        content: "What are the main data types in javaScript?",
+        answer: "String, number, bigint, boolean, undefined",
+        choices: [
+            "Decimal, integer, undefined, function, long",
+            "String, number, bigint, boolean, undefined",
+            "Var, string, symbol, object, small",
+            "console, form, object, loop"
+        ]
+    },
+    {
+        content: "What is the 'this' keyword?",
+        answer: "This refers to the object that a function is a property of.",
         choices: [
             "3",
             "0",
-            "14",
+            "This refers to the object that a function is a property of.",
             "6"
+        ]
+    },
+    {
+        content: "What are the main data types in javaScript?",
+        answer: "String, number, bigint, boolean, undefined",
+        choices: [
+            "Decimal, integer, undefined, function, long",
+            "String, number, bigint, boolean, undefined",
+            "Var, string, symbol, object, small",
+            "console, form, object, loop"
+        ]
+    },
+    {
+        content: "What is the 'this' keyword?",
+        answer: "This refers to the object that a function is a property of.",
+        choices: [
+            "3",
+            "0",
+            "This refers to the object that a function is a property of.",
+            "6"
+        ]
+    },
+    {
+        content: "What are the main data types in javaScript?",
+        answer: "String, number, bigint, boolean, undefined",
+        choices: [
+            "Decimal, integer, undefined, function, long",
+            "String, number, bigint, boolean, undefined",
+            "Var, string, symbol, object, small",
+            "console, form, object, loop"
         ]
     }
 ]
@@ -125,17 +193,20 @@ var questions = [
 startButton.addEventListener('click', start);
 
 function start() {
+
+    questionsLeft = questions.length - 1;
+    toAnswer.textContent = questionsLeft;
     startButton.remove();
     introParagraph.remove();
 
+    getQuestion();
     quizRunning();
 
 }
 
+
 function getQuestion() {
-
-    if (questionIndex < questions.length) {
-
+    var answer = questions[questionIndex].answer;
     question.innerHTML = questions[questionIndex].content;
 
     option1.innerHTML = questions[questionIndex].choices[0];
@@ -143,31 +214,31 @@ function getQuestion() {
     option3.innerHTML = questions[questionIndex].choices[2];
     option4.innerHTML = questions[questionIndex].choices[3];
 
-    optionButtons.append(option1);
-    optionButtons.append(option2);
-    optionButtons.append(option3);
-    optionButtons.append(option4);
+    optionButtons.appendChild(option1);
+    optionButtons.appendChild(option2);
+    optionButtons.appendChild(option3);
+    optionButtons.appendChild(option4);
 
-    option1.addEventListener('click', function (e) {
-        e.stopPropagation();
-        isCorrect(option1);
+    option1.addEventListener('click', function (event) {
+        event.preventDefault();
+        isCorrect(option1.innerText);
     });
-    option2.addEventListener('click', function (e) {
-        e.stopPropagation();
-        isCorrect(option2);
+    option2.addEventListener('click', function (event) {
+        event.preventDefault();
+        isCorrect(option2.innerText);
     });
-    option3.addEventListener('click', function (e) {
-        e.stopPropagation();
-        isCorrect(option3);
+    option3.addEventListener('click', function (event) {
+        event.preventDefault();
+        isCorrect(option3.innerText);
     });
-    option4.addEventListener('click', function (e) {
-        e.stopPropagation();
-        isCorrect(option4);
+    option4.addEventListener('click', function (event) {
+        event.preventDefault();
+        isCorrect(option4.innerText);
     });
-}
-else {
-submitHighscore();
-}
+
+    if (questionsLeft <= 0) {
+        submitHighscore();
+    }
 }
 
 function quizRunning() {
@@ -175,45 +246,61 @@ function quizRunning() {
     function decrementSeconds() {
         seconds -= 1;
         timeLeft.innerText = seconds;
+        if (seconds < 0) {
+            return;
+        }
     }
-
-    var cancel = setInterval(decrementSeconds, 1000);
-
-    getQuestion();
+    var countDown = setInterval(decrementSeconds, 1000);
+    
 }
 
 function isCorrect(selected) {
-
-    var choice = selected.innerHTML;
+    
     var answer = questions[questionIndex].answer;
+    
+        
 
-    if (choice.content == answer.content) {
+    if (selected == answer) {
         rightAnswer();
     } else {
         wrongAnswer();
     }
+    
 
 }
 
 function rightAnswer() {
     choiceResult.innerHTML = "Correct!";
-    incrementIndex();
+    questionsLeft -=1;
+    questionIndex +=1;
+    toAnswer.textContent = questionsLeft;
+    getQuestion();
     
 }
 function wrongAnswer() {
     
     choiceResult.innerHTML = "Wrong!";
+    questionsLeft -=1;
+    questionIndex +=1;
+    toAnswer.textContent = questionsLeft;
     seconds -= 10;
-    incrementIndex();
+    getQuestion();
 
 }
-function incrementIndex() {
-    questionIndex += 1;
-    getQuestion();
-}
+
 
 function submitHighscore() {
-    window.alert("Quiz over")
+    question.remove();
+    toAnswer.remove();
+    
+    option1.remove();
+    option2.remove();
+    option3.remove();
+    option4.remove();
+    toAnswer.innerHTML('');
+    choiceResult.innerHTML('');
+    submitButton.style.display = "inline-block";
+    submitButton.appendChild(submitArea);
 
 }
 
